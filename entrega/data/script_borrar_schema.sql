2cae213ee0dce14dc2a4fc780b7375d3084584f6
Leandro <llizaso@est.frba.utn.edu.ar>
2018-11-21 19:57:58 -0300

    Se agrega script para eliminar schema

---
+++ entrega/data/script_borrar_schema.sql
@@ -0,0 +1,44 @@
+drop table PEL.Item_Factura
+go
+drop table PEL.Ubicacion
+go
+drop table PEL.Tipo_Ubicacion
+go
+drop table PEL.Factura
+go
+drop table PEL.Compra
+go
+drop table PEL.Premio_Cliente
+go
+drop table PEL.Premio
+go
+drop table PEL.Empresa
+go
+drop table PEL.Cliente
+go
+drop table PEL.Publicacion
+go
+drop table PEL.Estado_Publicacion
+go
+drop table PEL.Rol_Usuario
+go
+drop table PEL.Rol_Funcion
+go
+drop table PEL.Funcion
+go
+drop table PEL.Usuario
+go
+drop table PEL.Rol
+go
+drop table PEL.Rubro
+go
+drop table PEL.Grado
+go
+drop function PEL.f_hash
+go
+drop procedure PEL.hash
+go
+drop function PEL.calcular_publ_estado
+go
+drop schema PEL
+go
