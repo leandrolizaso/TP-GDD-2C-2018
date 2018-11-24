a0df7978228a17f49d157e1d985b9b1c9cc11a5d
David Perez <davidperez@est.frba.utn.edu.ar>
2018-11-24 16:00:56 -0300

    Login completo (con procedure)

--- entrega/src/RegistroUsuario/LoginDAO.cs
+++ entrega/src/RegistroUsuario/LoginDAO.cs
@@ -13,10 +13,10 @@ namespace PalcoNet.RegistroUsuario {
             dict.Add("@password", pass);
 
             //PEL.validar_usuario(@username nvarchar(50),@password nvarchar(255),@usua_id numeric(18,0) output,@mensaje nvarchar(255)) 
-            //DataTable result = procedure("PEL.validar_usuario", dict);
+            DataTable result = procedure("PEL.validar_usuario", dict);
 
-            Decimal idUsuario = 1; //(Decimal)result.Rows[0]["@usua_id"];
-            string mensaje = "ta todo bien"; //result.Rows[0]["@mensaje"].ToString();
+            Decimal idUsuario = Convert.ToDecimal(result.Rows[0][0]);
+            string mensaje = result.Rows[0][1].ToString();
 
             if (idUsuario < 0) {
                 throw new ArgumentException(mensaje);
