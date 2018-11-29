47db576d60c670bacec0011826f14d0db61d9936
Leandro <llizaso@est.frba.utn.edu.ar>
2018-11-29 17:17:05 -0300

    Creo pantallas canje de puntos

---
+++ entrega/src/CanjePuntos/CanjePremio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PalcoNet.CanjePuntos
+{
+    public partial class CanjePremio : Form
+    {
+        public CanjePremio()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void datagrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CanjePremio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
