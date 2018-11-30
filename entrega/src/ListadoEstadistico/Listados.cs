c68b0ec1f7895ad8d00946c17ac8e0f3a45d973c
Leandro <llizaso@est.frba.utn.edu.ar>
2018-11-30 20:47:08 -0300

    Agrego argumentos cuando abro los forms

--- entrega/src/ListadoEstadistico/Listados.cs
+++ entrega/src/ListadoEstadistico/Listados.cs
@@ -9,32 +9,40 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PalcoNet.ListadoEstadistico
-{
+{   
+    
+
     public partial class listados : Form
-    {
-        public listados()
+    {   
+        string fechaDesde;
+        string fechaHasta;
+
+        public listados(string fechaDesde, string fechaHasta)
         {
             InitializeComponent();
+            this.fechaDesde = fechaDesde;
+            this.fechaHasta = fechaHasta;
         }
 
         private void localidades_Click(object sender, EventArgs e)
         {
             this.Hide();
-            new ListadoEmpresas().ShowDialog();
+            new ListadoEmpresas(fechaDesde).ShowDialog();
+                                //Pongo una sola porque se supone que es por un solo mes
             this.Show();
         }
 
         private void puntos_Click(object sender, EventArgs e)
         {
             this.Hide();
-            new ListadoPuntos().ShowDialog();
+            new ListadoPuntos(fechaDesde, fechaHasta).ShowDialog();
             this.Show();
         }
 
         private void compras_Click(object sender, EventArgs e)
         {
             this.Hide();
-            new ListadoCompras().ShowDialog();
+            new ListadoCompras(fechaDesde, fechaHasta).ShowDialog();
             this.Show();
         }
     }
