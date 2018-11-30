fe8e3b59d742b5fe2d0e32d7b115f4d97d3670b0
David Perez <davidperez@est.frba.utn.edu.ar>
2018-11-30 18:07:42 -0300

    Fechas helper

---
+++ entrega/src/Utils/Fechas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PalcoNet.Utils {
+    public static class Fechas {
+
+        public static DateTime[] getRango(int trimestre, int año) {
+            if (trimestre < 1 || trimestre > 4) {
+                throw new ArgumentOutOfRangeException("El trimestre debe ser uno de: 1,2,3 o 4");
+            }
+            var desde = new DateTime(año,(trimestre-1)*3+1,1);
+            var hasta = new DateTime(año, (trimestre - 1) * 3 + 3, 1);
+            return new DateTime[]{desde,hasta};
+        }
+    }
+}
